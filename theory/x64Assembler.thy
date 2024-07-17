7b1fa616f0ccf92f92ccdd4a46dd5b24a045d2b2
lujiayi <1276897336@qq.com>
2024-07-17 15:36:00 +0800

    assembler:orq_rr

--- theory/x64Assembler.thy
+++ theory/x64Assembler.thy
@@ -59,6 +59,17 @@ fun x64_assemble_one_instruction :: "instruction \<Rightarrow> x64_bin option" w
     let (op:: u8) = 0xf7 in
     let (rop::u8) = construct_modsib_to_u8 0b11 0b011 (u8_of_ireg rd) in
       Some [rex, op, rop] |
+  \<comment> \<open> P2884 `OR register1 to register2` -> ` 0000 100w : 11 reg1 reg2` \<close>
+  Porq_rr rd r1  \<Rightarrow> 
+     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `0R0B` \<close>
+      True \<comment> \<open> W \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg rd) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0x09 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
+      Some [ rex, op, rop] |
   \<comment> \<open> P2884 `NOP – No Operation` -> `1001 0000` \<close>
   Pnop \<Rightarrow> Some [0x90] |
   _ \<Rightarrow> None
