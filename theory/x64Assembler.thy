96e7663c62a5f210d4d01eef58c51c1e864c3edb
lujiayi <1276897336@qq.com>
2024-07-19 03:08:20 +0800

    assembler:xorq_rr,xorl_rr

--- theory/x64Assembler.thy
+++ theory/x64Assembler.thy
@@ -72,7 +72,7 @@ fun x64_assemble_one_instruction :: "instruction \<Rightarrow> x64_bin option" w
       Some [ rex, op, rop] |
   \<comment> \<open> P2876 `AND register1 to register2` -> ` 0100 1R0B : 0000 100w : 11 reg1 reg2` \<close>
   Pandq_rr rd r1  \<Rightarrow> 
-     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `0R0B` \<close>
+     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `1R0B` \<close>
       True \<comment> \<open> W \<close>
       (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> R \<close>
       False \<comment> \<open> X \<close>
@@ -81,8 +81,28 @@ fun x64_assemble_one_instruction :: "instruction \<Rightarrow> x64_bin option" w
     let (op:: u8) = 0x21 in
     let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
       Some [ rex, op, rop] |
-  \<comment> \<open> P2893 `XOR register1 to register2` -> ` 0100 1RXB : 0011 000w : 11 reg1 reg2` \<close>
-
+  \<comment> \<open> P2893 `XOR qwordregister1 to qwordregister2` -> ` 0100 1R0B : 0011 000w : 11 reg1 reg2` \<close>
+  Pxorq_rr rd r1  \<Rightarrow> 
+     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `1R0B` \<close>
+      True \<comment> \<open> W \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg rd) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0x31 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
+      Some [ rex, op, rop] |
+  \<comment> \<open> P2893 `XOR register1 to register2` -> ` 0100 0RXB : 0011 000w : 11 reg1 reg2` \<close>
+  Pxorl_rr rd r1  \<Rightarrow> 
+     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `0R0B` \<close>
+      False \<comment> \<open> W \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg rd) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0x31 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
+      Some [ rex, op, rop] |
   \<comment> \<open> P2884 `NOP – No Operation` -> `1001 0000` \<close>
   Pnop \<Rightarrow> Some [0x90] |
   _ \<Rightarrow> None
