24e85a3430f6dce13d8945b3042397164d705b99
lujiayi <1276897336@qq.com>
2024-07-20 19:24:59 +0800

    assembler:mull_r,mulq_r,imull_r,imulq_r

--- theory/x64Assembler.thy
+++ theory/x64Assembler.thy
@@ -48,7 +48,7 @@ fun x64_assemble_one_instruction :: "instruction \<Rightarrow> x64_bin option" w
     let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
       Some [ rex, op, rop] |
   \<comment> \<open> P2884 `NEG register2` -> `0100 100B : 1111 011w : 11011reg` \<close>
-  Pnegq     rd \<Rightarrow>
+  Pnegq rd \<Rightarrow>
     \<comment> \<open> let (prefix:: u8) = 0x66 in\<close>
     let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `100B` \<close>
       True \<comment> \<open> W \<close>
@@ -102,6 +102,50 @@ fun x64_assemble_one_instruction :: "instruction \<Rightarrow> x64_bin option" w
       ) in
     let (op:: u8) = 0x31 in
     let (rop::u8) = construct_modsib_to_u8 0b11 (u8_of_ireg r1) (u8_of_ireg rd) in
+      Some [ rex, op, rop] |
+  \<comment> \<open> P2884 `MUL AL, AX, or EAX with register2` -> ` 0100 000B : 1111 100w : 11 reg1 reg2` \<close>
+  Pmull_r r1 \<Rightarrow>
+    let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `000B` \<close>
+      False \<comment> \<open> W \<close>
+      False \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0xf7 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 0b100 (u8_of_ireg r1) in
+      Some [ rex, op, rop] |
+ \<comment> \<open> P2884 `MUL RAX with qwordregister (to RDX:RAX)` -> ` 0100 100B : 1111 100w : 11 reg1 reg2` \<close>
+  Pmulq_r r1 \<Rightarrow>
+    let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `100B` \<close>
+      True \<comment> \<open> W \<close>
+      False \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0xf7 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 0b100 (u8_of_ireg r1) in
+      Some [ rex, op, rop] |
+  \<comment> \<open> P2884 `IMUL AL, AX, or EAX with register2` -> ` 0100 000B : 1111 101w : 11 reg1 reg2` \<close>
+  Pimull_r r1 \<Rightarrow>
+    let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `000B` \<close>
+      False \<comment> \<open> W \<close>
+      False \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0xf7 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 0b101 (u8_of_ireg r1) in
+      Some [ rex, op, rop] |
+ \<comment> \<open> P2880 `IMUL RAX with qwordregister (to RDX:RAX)` -> ` 0100 100B : 1111 101w : 11 reg1 reg2` \<close>
+  Pimulq_r r1 \<Rightarrow>
+    let (rex:: u8) = or 0x40 (construct_rex_to_u8  \<comment> \<open> `100B` \<close>
+      True \<comment> \<open> W \<close>
+      False \<comment> \<open> R \<close>
+      False \<comment> \<open> X \<close>
+      (and (u8_of_ireg r1) 0b1000 \<noteq> 0) \<comment> \<open> B \<close>
+      ) in
+    let (op:: u8) = 0xf7 in
+    let (rop::u8) = construct_modsib_to_u8 0b11 0b101 (u8_of_ireg r1) in
       Some [ rex, op, rop] |
   \<comment> \<open> P2884 `NOP – No Operation` -> `1001 0000` \<close>
   Pnop \<Rightarrow> Some [0x90] |
