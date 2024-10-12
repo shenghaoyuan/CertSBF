5e47fd28142241f04640e6efcf2f952b543d6783
zhuoruo <12399655@qq.com>
2024-10-12 12:35:10 +0800

    update div proof

--- theory/bpfConsistencyAux1.thy
+++ theory/bpfConsistencyAux1.thy
@@ -184,7 +184,7 @@ lemma  div_subgoal_rr_aux2_3:"xins = Pdivq_r (bpf_to_x64_reg src) \<Longrightarr
 
 lemma  div_subgoal_rr_aux2_4:"xins = Ppopl x64Syntax.RDX \<Longrightarrow> 
     Next reg' m' = (exec_instr xins 1 reg m)\<Longrightarrow> 
-    \<forall> r. (bpf_to_x64_reg r) \<notin> {x64Syntax.RDX, x64Syntax.RSP} \<longrightarrow> reg' (IR (bpf_to_x64_reg r)) = reg (IR (bpf_to_x64_reg r))"
+    \<forall> r. r  \<notin> {x64Syntax.RDX, x64Syntax.RSP} \<longrightarrow> reg' (IR r) = reg (IR r)"
   apply(unfold exec_instr_def )
   apply(cases xins, simp_all)
   apply(unfold exec_pop_def Let_def)
@@ -369,15 +369,85 @@ lemma div_subgoal_rr_aux8_1:
   assumes a0:"xins = [Ppushl_r x64Syntax.RDX, Pxorq_rr x64Syntax.RDX x64Syntax.RDX,Pdivq_r (bpf_to_x64_reg src),Ppopl x64Syntax.RDX]" and 
     a1:"Next reg'' m'' = interp3 xins (Next reg m)" and
     a2:"Next reg1 m1 = exec_instr (xins!0) sz reg m " and
-    a3:"Next reg2 m2 = exec_instr (xins!1) sz reg m " 
-  shows " reg'' (IR x64Syntax.RAX) = reg2 (IR x64Syntax.RAX)"
+    a3:"Next reg2 m2 = exec_instr (xins!1) sz reg1 m1 " and
+    a4:"Next reg3 m3 = exec_instr (xins!2) sz reg2 m2" and
+    a5:"Next reg' m' = exec_instr xins2 1 tmpreg m" and
+    a6:"xins2 = Pdivq_r (bpf_to_x64_reg src)" and
+    a7:"tmpreg (IR x64Syntax.RAX) = reg (IR x64Syntax.RAX)" and
+    a8:"tmpreg (IR x64Syntax.RDX) = Vlong 0"
+  shows"reg' (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)"
   sorry
+  (*shows " reg'' (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)"*)
+(*proof-
+  have b0_0:"length xins = 4" using a0 by simp
+  have b0_1:"(xins!2) = xins2" using a0 a6 by simp
+  have b0_2:"length xins >0" using a0 by simp
+  have b0_3:"(xins!0) = Ppushl_r x64Syntax.RDX" using a0 by simp
+  have b1:"reg1 (IR x64Syntax.RAX) = reg (IR x64Syntax.RAX)" using a0 b0_3 a2 div_subgoal_rr_aux2_1 nth_Cons_0 a3 sorry
+  have b2:"reg1 (IR x64Syntax.RAX) = reg2 (IR x64Syntax.RAX)" using a0 b0_3 a2 div_subgoal_rr_aux2_1 nth_Cons_0 a3 sorry
+  have b3:"reg2 (IR x64Syntax.RAX) = reg (IR x64Syntax.RAX)" using b1 b2 by simp
+  have ""
+  qed
+*)
+
+lemma div_subgoal_rr_aux8_3:
+  assumes a0:"last xins = Pdivq_r (bpf_to_x64_reg src)"and
+          a1:"interp3 (butlast xins) (Next reg m) = Next reg' m'"and
+          a2:"Next reg'' m'' = (exec_instr (last xins) 1 reg' m') " and
+          a3:"reg' (IR x64Syntax.RDX) = Vlong 0" and
+          a4:"reg' (IR x64Syntax.RAX) =  reg (IR x64Syntax.RAX)" and
+          a5:"Next tmpreg' m'' = exec_instr (last xins) 1 tmpreg m'" and
+          a6: "tmpreg (IR x64Syntax.RDX) = Vlong 0" and
+          a7:"tmpreg (IR x64Syntax.RAX) = reg (IR x64Syntax.RAX)"
+        shows "reg'' (IR x64Syntax.RAX) = tmpreg' (IR x64Syntax.RAX)"
+  sorry
+(*proof-
+  let ?midlist = "butlast(tl xins)"
+  have b0_1:"xins = [Ppushl_r x64Syntax.RCX]@?midlist@[Ppopl x64Syntax.RCX]" using a0 a1
+    by (metis append_Cons append_Nil append_butlast_last_id hd_Nil_eq_last instruction.distinct(5787) last_ConsL last_tl list.collapse)
+  have b0:"Next reg' m' = exec_push 1 M32 m reg (reg (IR x64Syntax.RCX))" using exec_instr_def a0 
+    using exec_push_def a5 a0 
+    by (smt (z3) a1 hd_Nil_eq_last hd_conv_nth instruction.distinct(5787) instruction.simps(6295))
+  have b1:"Next reg' m' =
+    (case storev M32 m (sub64 (reg (IR SP)) (vlong_of_memory_chunk M32)) (reg (IR ireg.RCX)) of None \<Rightarrow> Stuck
+     | Some (x::64 word \<Rightarrow> 8 word option option) \<Rightarrow>
+         Next
+          ((undef_regs [CR ZF, CR CF, CR PF, CR SF, CR OF] (reg(IR SP := sub64 (reg (IR SP)) (vlong_of_memory_chunk M32))))
+           (RIP := add64 (undef_regs [CR ZF, CR CF, CR PF, CR SF, CR OF] (reg(IR SP := sub64 (reg (IR SP)) (vlong_of_memory_chunk M32))) RIP) (Vlong ((1::64 word) + (1::64 word)))))
+          x) " using b0 nextinstr_nf_def nextinstr_def exec_push_def by metis
+  have b2_1:"(exec_instr (xins!0) 1 reg m) = Next reg' m' \<longrightarrow> storev M32 m (sub64 (reg (IR SP)) (vlong_of_memory_chunk M32)) (reg (IR ireg.RCX)) \<noteq> None" using push_pop_subgoal_rr_aux1 a0 a1 
+    by (metis b1 option.case(1) outcome.simps(3))
+  have b2_2:"storev M32 m (sub64 (reg (IR SP)) (vlong_of_memory_chunk M32)) (reg (IR ireg.RCX)) \<noteq> None" using b2_1 a5 by simp
+  have b2:"storev M32 m (sub64 (reg (IR SP)) (vlong_of_memory_chunk M32)) (reg (IR ireg.RCX))= Some m'" using b1 b2_2 by auto
+  have b3:"reg' (IR SP) = sub64 (reg (IR SP)) (vlong_of_memory_chunk M32)" using b1 b2 by auto
+  let "?sp" = "reg' (IR SP)"
+  have b4:"storev M32 m (reg' (IR SP)) (reg (IR ireg.RCX)) = Some m'" using b2 b3 by simp
+  have b5:"interp3 (butlast(xins)) (Next reg m) = Next reg2 m'" using a2 a5 b0_1 a0
+    by (metis append_Cons append_Nil butlast.simps(1) butlast.simps(2) interp3.simps(2) list.sel(3) list.simps(3) nth_Cons_0 outcome.case(1))
+  have b6:"(exec_instr (last xins) 1 reg2 m') = Next reg'' m''" using a5 a1 a2 a3 b5 by simp
+  have c0:"Next reg'' m'' = exec_pop (1::64 word) M32 m' reg2 (IR ireg.RCX)" using exec_instr_def using b6 a1 by simp
+  have c1:"loadv M32 m' (reg' (IR SP)) = Some (reg (IR ireg.RCX))" using b4 store_load_consistency by simp
+  have c2:"loadv M32 m' (reg2 (IR SP)) = Some (reg (IR ireg.RCX))" using b4 c1 a6 by simp
+  let "?v" = " (reg (IR ireg.RCX))"
+  have c3:" Next reg'' m'' =
+    (case loadv M32 m' (reg2 (IR SP)) of None \<Rightarrow> Stuck
+     | Some (v::val) \<Rightarrow>
+         Next
+          ((undef_regs [CR ZF, CR CF, CR PF, CR SF, CR OF] (reg2(IR ireg.RCX := ?v, IR SP := add64 (reg2 (IR SP)) (vlong_of_memory_chunk M32))))
+           (RIP := add64 (undef_regs [CR ZF, CR CF, CR PF, CR SF, CR OF] (reg2(IR ireg.RCX := ?v, IR SP := add64 (reg2 (IR SP)) (vlong_of_memory_chunk M32))) RIP) (Vlong ((1::64 word) + (1::64 word)))))
+          m')" using nextinstr_nf_def nextinstr_def exec_pop_def 
+    using c0 c2 by simp
+  have c4:"reg''(IR ireg.RCX) = ?v" using exec_pop_def c2 c3 by simp
+  thus ?thesis using c4 by simp
+qed
+*)
 
 lemma div_subgoal_rr_aux8_2:
   assumes a0:"xins = [Ppushl_r x64Syntax.RDX, Pxorq_rr x64Syntax.RDX x64Syntax.RDX,Pdivq_r (bpf_to_x64_reg src),Ppopl x64Syntax.RDX]" and 
     a1:"Next reg'' m'' = interp3 xins (Next reg m)" and
     a2:"Next reg' m' = exec_instr xins2 1 reg m" and
-    a3:"xins2 = Pdivq_r (bpf_to_x64_reg src)"
+    a3:"xins2 = Pdivq_r (bpf_to_x64_reg src)" and
+    a4:"reg (IR x64Syntax.RDX)  = Vlong 0"
   shows "reg' (IR x64Syntax.RAX) = reg'' (IR x64Syntax.RAX) "
 proof-
   have b0_0:"length xins = 4" using a0 by simp
@@ -395,12 +465,12 @@ proof-
   have b1_4:"Next reg2 m2 = interp3 (take 2 xins) (Next reg m)" using a0 b1_2 b1_3 b0_4
     by (metis append_Cons append_Nil interp3.simps(2) interp3_list_aux3 outcome.case(1))
   have b1_5:" Next reg2 m2 = interp3 (take 2 xins) (Next reg m) \<and> Next reg'' m'' = interp3 (drop 2 xins) (Next reg2 m2)" using interp3_length4_aux4 b0_0 a1 b1_4 by metis
-  have b1:"reg2 (IR x64Syntax.RAX) = reg1 (IR x64Syntax.RAX)" using a0 b1_2 a3 b0
+  have b1:"reg2 (IR x64Syntax.RAX) = reg1 (IR x64Syntax.RAX)" using a0 b1_2 
     using One_nat_def insertCI nth_Cons_0 nth_Cons_Suc div_subgoal_rr_aux2_2
     by (metis insert_absorb insert_iff insert_not_empty ireg.distinct(13) ireg.distinct(5))
   have b2_1:"\<exists> reg3 m3. Next reg3 m3 = (exec_instr (xins!2) 1 reg2 m2)" 
-    using a0 a3 a1 exec_instr_def 
-    by (smt (verit) Cons_nth_drop_Suc One_nat_def add.right_neutral add_Suc_right b0_0 b0_2 b1_5 drop0 interp3_length2_aux1 list.sel(3) list.size(3) list.size(4) nth_Cons_Suc numeral_2_eq_2 one_less_numeral_iff semiring_norm(76))
+    using a0 a3 a1 exec_instr_def sorry
+    (*by (smt (verit) Cons_nth_drop_Suc One_nat_def add.right_neutral add_Suc_right b0_0 b0_2 b1_5 drop0 interp3_length2_aux1 list.sel(3) list.size(3) list.size(4) nth_Cons_Suc numeral_2_eq_2 one_less_numeral_iff semiring_norm(76))*)
   then obtain reg3 m3 where b2_2:"Next reg3 m3 = (exec_instr (xins!2) 1 reg2 m2)" using b1_1 by auto
   have b2_3:"take 3 xins = [xins!0]@[xins!1]@[xins!2]" using a0 
     by (simp add: One_nat_def Suc_1 add_One numeral_3_eq_3 numeral_nat(2) take_Suc_conv_app_nth)
@@ -415,12 +485,17 @@ proof-
   let ?tmprax = "reg3 (IR x64Syntax.RAX)"
   have b3_1:" \<exists> reg4 m4. Next reg4 m4 = (exec_instr (xins!3) 1 reg3 m3)" 
     using b2_6 b2_9 by auto
-  then obtain reg4 m4 where b4:"Next reg4 m4 = (exec_instr (xins!3) 1 reg3 m3)" using b3_1 by auto
+  then obtain reg4 m4 where b3_2:"Next reg4 m4 = (exec_instr (xins!3) 1 reg3 m3)" using b3_1 by auto
   have b3:"Next reg4 m4 = Next reg'' m''" using interp3_list_aux3
-    by (simp add: b2_6 b2_9 b4)
-  have b4:"reg4 (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)" using a0 b0 b1 div_subgoal_rr_aux2_4 a0 b4
-    using append_Cons append_Nil b2_5 b2_9 list.sel(1) list.sel(3) b3 sorry
-  thus ?thesis using b0 b1 b3 sorry
+    by (simp add: b2_6 b2_9 b3_2)
+  have b4_1:"(xins!3) = Ppopl x64Syntax.RDX" using a0 by simp
+  have b4_2:"reg4 (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)" using a0 b0 b1 div_subgoal_rr_aux2_4 a0 b3_2 b4_1
+    using insert_absorb insert_iff insert_not_empty ireg.distinct(13) ireg.distinct(5) by simp   
+  have b4:"reg'' (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)" using b4_2 b3 by simp
+  have b5_1:"reg2 (IR x64Syntax.RAX) = reg (IR x64Syntax.RAX)" using b1 b0 by simp
+  (*have b5_2:"reg2 (IR x64Syntax.RDX) = Vlong 0" should be proved*)
+  have b5:" reg' (IR x64Syntax.RAX) = reg3 (IR x64Syntax.RAX)" using b5_1 a0 a1 a3 a2 div_subgoal_rr_aux8_1 b0_1 b1_2 b2_2 using b0_4 a4 by blast
+  thus ?thesis using b5 b4 by auto
 qed
 
 
@@ -458,7 +533,7 @@ lemma ucast_for_div:"(x::u64) div (y::u64) = ucast(((ucast x)::u128) div ((ucast
 lemma div_subgoal_rr_aux100_1:
     "bins = BPF_ALU64 BPF_DIV dst (SOReg src) \<Longrightarrow> 
     xins = Pdivq_r ri \<Longrightarrow>
-    (BPF_OK pc rs' m' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
+    (BPF_OK pc rs' mem' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
     Next reg' m' = exec_instr xins 1 reg m \<Longrightarrow>
     x64Syntax.RAX = (bpf_to_x64_reg dst) \<Longrightarrow> 
     ri = (bpf_to_x64_reg src) \<Longrightarrow> 
@@ -492,7 +567,7 @@ lemma div_subgoal_rr_aux100_1:
 lemma div_subgoal_rr_aux100_2:
     "bins = BPF_ALU64 BPF_DIV dst (SOReg src) \<Longrightarrow> 
     xins = Pdivq_r ri \<Longrightarrow>
-    (BPF_OK pc rs' m' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
+    (BPF_OK pc rs' mem' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
     Next reg' m' = exec_instr xins 1 reg m \<Longrightarrow>
     x64Syntax.RAX = (bpf_to_x64_reg dst) \<Longrightarrow> 
     ri = (bpf_to_x64_reg src) \<Longrightarrow> 
@@ -513,13 +588,11 @@ lemma div_subgoal_rr_aux100_2:
 
 lemma div_subgoal_rr_aux100_3:
     "bins = BPF_ALU64 BPF_DIV dst (SOReg src) \<Longrightarrow> 
-    xins = Pdivq_r ri \<Longrightarrow>
-    (BPF_OK pc rs' m' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
-    Next reg' m' = exec_instr xins 1 reg m \<Longrightarrow>
+    (BPF_OK pc rs' mem' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu \<Longrightarrow> 
+    Next reg' m' = exec_instr (Pdivq_r (bpf_to_x64_reg src)) 1 reg m \<Longrightarrow>
     x64Syntax.RAX = (bpf_to_x64_reg dst) \<Longrightarrow> 
-    ri = (bpf_to_x64_reg src) \<Longrightarrow> 
     reg (IR x64Syntax.RAX) = Vlong n1 \<Longrightarrow> 
-    reg (IR ri) = Vlong n2 \<Longrightarrow> 
+    reg (IR (bpf_to_x64_reg src)) = Vlong n2 \<Longrightarrow> 
     n1 = rs dst \<Longrightarrow> 
     n2  = rs src \<Longrightarrow> 
     reg (IR x64Syntax.RDX) = Vlong 0 \<Longrightarrow>
@@ -535,12 +608,11 @@ lemma reg_rax_rdx1:"(bpf_to_x64_reg dst) = x64Syntax.RAX \<longrightarrow> (bpf_
 lemma div_subgoal_rr_aux100:
   assumes a0:"bins = BPF_ALU64 BPF_DIV dst (SOReg src)" and
      a1:"xins = [Ppushl_r x64Syntax.RDX, Pxorq_rr x64Syntax.RDX x64Syntax.RDX,Pdivq_r (bpf_to_x64_reg src),Ppopl x64Syntax.RDX]" and
-     a2:"(BPF_OK pc rs' m' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu" and
+     a2:"(BPF_OK pc rs' m' ss' is_v1 (cur_cu+1) remain_cu) = step fuel bins rs m ss is_v1 cur_cu remain_cu " and
      a3:"Next reg' m' = interp3 xins (Next reg m)" and
-     a4:"(bpf_to_x64_reg dst) = x64Syntax.RAX" and
-     a5:"ri = (bpf_to_x64_reg src)" and
+     a4:"x64Syntax.RAX = (bpf_to_x64_reg dst)" and
      a6:"reg (IR x64Syntax.RAX) = Vlong n1" and
-     a7:"reg (IR ri) = Vlong n2" and
+     a7:"reg (IR (bpf_to_x64_reg src)) = Vlong n2" and
      a8:"n1  = rs dst" and
      a9:"n2  = rs src" and
      a10:"reg (IR x64Syntax.RDX) = Vlong 0"
@@ -554,7 +626,7 @@ proof-
   have b1_2:"result  \<noteq> Stuck " using interp3_length4_aux0 a3 b0_0 by (metis b1_1)
   hence b1_3:"\<exists> tmpreg tmpm. result = Next tmpreg tmpm" using b1_2 b1_1 by (meson outcome.exhaust)
     then obtain tmpreg tmpm where b1:"Next tmpreg tmpm = exec_instr (Pdivq_r (bpf_to_x64_reg src)) 1 reg m" using b1_3 b1_1 b0 by auto
-    have b2:"Vlong (rs' dst) = tmpreg (IR (bpf_to_x64_reg dst))" using div_subgoal_rr_aux100_3 using exec_instr_def a0 b0 a2 a4 a5 a6 a7 a8 a9 a10 b1 sorry
+    have b2:"Vlong (rs' dst) = tmpreg (IR x64Syntax.RAX)" using div_subgoal_rr_aux100_3 using a0 b0 a2 a4 a6 a7 a8 a9 a10 b1 by simp
     have b3_0:"Vlong (rs' dst) = tmpreg (IR (x64Syntax.RAX))" using b2 a4 by simp
     have b3:"Vlong (rs' dst)  =  reg' (IR x64Syntax.RAX)" using div_subgoal_rr_aux8_2 b0 a3 b3_0 b1 a1 by metis
     thus ?thesis using a4 by simp
@@ -570,7 +642,7 @@ lemma divq_subgoal_rr_generic:
        a6:"(\<forall> r. Vlong (rs r) = reg (IR (bpf_to_x64_reg r)))" and
        a7:"(bpf_to_x64_reg dst) = x64Syntax.RAX" and
        a8:"xins = map snd v" and
-       a9:"reg (IR x64Syntax.RDX) = Vlong 0"
+       a9:"reg (IR x64Syntax.RDX) = Vlong 0"(*should be proved*)
   shows "(\<forall> r. Vlong (rs' r) = reg' (IR (bpf_to_x64_reg r)))"
 proof -
   have b:"Some bl = x64_encodes_suffix [Ppushl_r x64Syntax.RDX, Pxorq_rr (x64Syntax.RDX) (x64Syntax.RDX),
